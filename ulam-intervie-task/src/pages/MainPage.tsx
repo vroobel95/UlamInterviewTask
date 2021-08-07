d3d249f7c23fbc47aac514b7e94783ce2c71ba41
vroobel95 <vroobel95@users.noreply.github.com>
2021-08-07 22:19:24 +0200

    Creating popup, changing folders

--- ulam-intervie-task/src/pages/MainPage.tsx
+++ ulam-intervie-task/src/pages/MainPage.tsx
@@ -1,16 +1,21 @@
-import './MainPage.scss';
+import "./MainPage.scss";
 
-import React from "react";
-import Dropzone from "../components/Dropzone";
+import React, { useState } from "react";
+import Dropzone from "../components/shared/Dropzone";
 import Map from "../components/Map";
+import PopUp from "../components/shared/PopUp";
+import ChooseColumnForm from "../components/forms/ChooseColumnForm";
 
 // MAPS_API_KEY = 'AIzaSyBZLDVwtsw8LPXxHVhSXRlFhmDe0SdmeRM';
 
 const MainPage: React.FC = () => {
+  const [isOpenPopup, setIsOpenPopup] = useState<boolean>(false);
+
   const handleDropFile = (files: File[]) => {
+    setIsOpenPopup(true);
     console.log("something");
   };
-
+  
   return (
     <div className="main-page">
       <div>{"Check the map"}</div>
@@ -20,6 +25,9 @@ const MainPage: React.FC = () => {
           { latitude: 51.108594, longitude: 16.978344, color: "#ef0823" },
         ]}
       />
+        <PopUp isOpen={isOpenPopup} onClose={() => setIsOpenPopup(false)}>
+          <ChooseColumnForm />
+        </PopUp>
     </div>
   );
 };
