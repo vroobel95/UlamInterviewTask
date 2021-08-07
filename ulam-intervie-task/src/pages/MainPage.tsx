8f4ea05356348af219d660ec36b1d9a749c26f12
vroobel95 <vroobel95@users.noreply.github.com>
2021-08-07 20:53:10 +0200

    Dropzone, styles

--- ulam-intervie-task/src/pages/MainPage.tsx
+++ ulam-intervie-task/src/pages/MainPage.tsx
@@ -1,12 +1,20 @@
+import './MainPage.scss';
+
 import React from "react";
+import Dropzone from "../components/Dropzone";
 import Map from "../components/Map";
 
 // MAPS_API_KEY = 'AIzaSyBZLDVwtsw8LPXxHVhSXRlFhmDe0SdmeRM';
 
 const MainPage: React.FC = () => {
+  const handleDropFile = (files: File[]) => {
+    console.log("something");
+  };
+
   return (
     <div className="main-page">
       <div>{"Check the map"}</div>
+      <Dropzone onDrop={handleDropFile} />
       <Map
         locations={[
           { latitude: 51.108594, longitude: 16.978344, color: "#ef0823" },
